46f0637a6884ea265c3a6cfde5f5de525e096dd2
David Parise <parise@ualberta.ca>
2023-11-07 22:30:39 -0700

    Updated GoingMerry.cpp

--- GoingMerry.cpp
+++ GoingMerry.cpp
@@ -4,92 +4,333 @@
 using namespace sc2;
 using namespace std;
 
-void GoingMerry::OnGameStart() { return; }
-
-void GoingMerry::OnStep() 
-{ 
-    std::cout << Observation()->GetGameLoop() << std::endl;
-    TryBuildSupplyDepot();
-    TryBuildBarracks();
+size_t GoingMerry::CountUnitType(const ObservationInterface* observation, UnitTypeID unit_type) {
+    return observation->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
 }
 
-
-void GoingMerry::OnUnitIdle(const Unit* unit)
-{
-    switch (unit->unit_type.ToType())
-    {
-    case UNIT_TYPEID::TERRAN_COMMANDCENTER:
-    {
-        Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
-        break;
-    }
-    case UNIT_TYPEID::TERRAN_SCV: {
-        const Unit* mineral_target = FindNearestMineralPatch(unit->pos);
-        if (!mineral_target) {
-            break;
+//An estimate of how many workers we should have based on what buildings we have
+int GoingMerry::GetExpectedWorkers(UNIT_TYPEID vespene_building_type) {
+    const ObservationInterface* observation = Observation();
+    Units bases = observation->GetUnits(Unit::Alliance::Self, IsTownHall());
+    Units geysers = observation->GetUnits(Unit::Alliance::Self, IsUnit(vespene_building_type));
+    int expected_workers = 0;
+    for (const auto& base : bases) {
+        if (base->build_progress != 1) {
+            continue;
         }
-        Actions()->UnitCommand(unit, ABILITY_ID::SMART, mineral_target);
-        break;
-    }
-    case UNIT_TYPEID::TERRAN_BARRACKS: {
-        Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_MARINE);
-        break;
-    }
-    case UNIT_TYPEID::TERRAN_MARINE: {
-        const GameInfo& game_info = Observation()->GetGameInfo();
-        Actions()->UnitCommand(unit, ABILITY_ID::ATTACK_ATTACK, game_info.enemy_start_locations.front());
-        break;
+        expected_workers += base->ideal_harvesters;
     }
 
-    default:
-    {
-        break;
+    for (const auto& geyser : geysers) {
+        if (geyser->vespene_contents > 0) {
+            if (geyser->build_progress != 1) {
+                continue;
+            }
+            expected_workers += geyser->ideal_harvesters;
+        }
     }
-    }
+
+    return expected_workers;
 }
 
-size_t GoingMerry::CountUnitType(UNIT_TYPEID unit_type) {
-    return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
-}
-
-bool GoingMerry::TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type) {
+// Same as above with expansion timings
+bool GoingMerry::TryBuildExpansionNexus() {
     const ObservationInterface* observation = Observation();
 
-    // If a unit already is building a supply structure of this type, do nothing.
-    // Also get an scv to build the structure.
-    const Unit* unit_to_build = nullptr;
-    Units units = observation->GetUnits(Unit::Alliance::Self);
-    for (const auto& unit : units) {
-        for (const auto& order : unit->orders) {
+    //Don't have more active bases than we can provide workers for
+    if (GetExpectedWorkers(UNIT_TYPEID::PROTOSS_ASSIMILATOR) > max_worker_count) {
+        return false;
+    }
+    // If we have extra workers around, try and build another nexus.
+    if (GetExpectedWorkers(UNIT_TYPEID::PROTOSS_ASSIMILATOR) < observation->GetFoodWorkers() - 16) {
+        return TryExpand(ABILITY_ID::BUILD_NEXUS, UNIT_TYPEID::PROTOSS_PROBE);
+    }
+    //Only build another nexus if we are floating extra minerals
+    if (observation->GetMinerals() > CountUnitType(observation, UNIT_TYPEID::PROTOSS_NEXUS) * 400) {
+        return TryExpand(ABILITY_ID::BUILD_NEXUS, UNIT_TYPEID::PROTOSS_PROBE);
+    }
+    return false;
+}
+
+//Try build structure given a location. This is used most of the time
+bool GoingMerry::TryBuildStructure(AbilityID ability_type_for_structure, UnitTypeID unit_type, Point2D location, bool isExpansion) {
+
+    const ObservationInterface* observation = Observation();
+    Units workers = observation->GetUnits(Unit::Alliance::Self, IsUnit(unit_type));
+
+    //if we have no workers Don't build
+    if (workers.empty()) {
+        return false;
+    }
+
+    // Check to see if there is already a worker heading out to build it
+    for (const auto& worker : workers) {
+        for (const auto& order : worker->orders) {
             if (order.ability_id == ability_type_for_structure) {
                 return false;
             }
         }
-        if (unit->unit_type == unit_type) {
-            unit_to_build = unit;
-        }
     }
 
-    float rx = GetRandomScalar();
-    float ry = GetRandomScalar();
+    // If no worker is already building one, get a random worker to build one
+    const Unit* unit = GetRandomEntry(workers);
 
-    Actions()->UnitCommand(unit_to_build,
-        ability_type_for_structure,
-        Point2D(unit_to_build->pos.x + rx * 15.0f, unit_to_build->pos.y + ry * 15.0f));
-
-    return true;
+    // Check to see if unit can make it there
+    if (Query()->PathingDistance(unit, location) < 0.1f) {
+        return false;
+    }
+    if (!isExpansion) {
+        for (const auto& expansion : expansions) {
+            if (Distance2D(location, Point2D(expansion.x, expansion.y)) < 7) {
+                return false;
+            }
+        }
+    }
+    // Check to see if unit can build there
+    if (Query()->Placement(ability_type_for_structure, location)) {
+        Actions()->UnitCommand(unit, ability_type_for_structure, location);
+        return true;
+    }
+    return false;
 
 }
 
-bool GoingMerry::TryBuildSupplyDepot() {
+//Try to build a structure based on tag, Used mostly for Vespene, since the pathing check will fail even though the geyser is "Pathable"
+bool GoingMerry::TryBuildStructure(AbilityID ability_type_for_structure, UnitTypeID unit_type, Tag location_tag) {
+    const ObservationInterface* observation = Observation();
+    Units workers = observation->GetUnits(Unit::Alliance::Self, IsUnit(unit_type));
+    const Unit* target = observation->GetUnit(location_tag);
+
+    if (workers.empty()) {
+        return false;
+    }
+
+    // Check to see if there is already a worker heading out to build it
+    for (const auto& worker : workers) {
+        for (const auto& order : worker->orders) {
+            if (order.ability_id == ability_type_for_structure) {
+                return false;
+            }
+        }
+    }
+
+    // If no worker is already building one, get a random worker to build one
+    const Unit* unit = GetRandomEntry(workers);
+
+    // Check to see if unit can build there
+    if (Query()->Placement(ability_type_for_structure, target->pos)) {
+        Actions()->UnitCommand(unit, ability_type_for_structure, target);
+        return true;
+    }
+    return false;
+
+}
+
+void GoingMerry::BuildOrder()
+{
+    const ObservationInterface* observation = Observation();
+    size_t n_gateway = CountUnitType(observation, UNIT_TYPEID::PROTOSS_GATEWAY) + CountUnitType(observation, UNIT_TYPEID::PROTOSS_WARPGATE);
+    size_t n_cybernetics = CountUnitType(observation, UNIT_TYPEID::PROTOSS_CYBERNETICSCORE);
+    size_t n_base = CountUnitType(observation, UNIT_TYPEID::PROTOSS_NEXUS);
+    size_t n_robotics_facility = CountUnitType(observation, UNIT_TYPEID::PROTOSS_ROBOTICSFACILITY);
+    size_t n_forge = CountUnitType(observation, UNIT_TYPEID::PROTOSS_FORGE);
+
+    if (n_gateway < min<size_t>(2 * n_base, 7))
+    {        
+        if (n_cybernetics < 1 && n_gateway > 0)
+        {
+            // Build cybernetics core if we have 1 gateway and no cybernetics cores
+            TryBuildStructureNearPylon(ABILITY_ID::BUILD_CYBERNETICSCORE, UNIT_TYPEID::PROTOSS_PROBE);
+            return;
+        }
+        else
+        {
+            if (n_base < 2 && n_gateway > 0)
+            {
+                TryBuildExpansionNexus();
+                return;
+            }
+
+            if (observation->GetFoodWorkers() >= target_worker_count && observation->GetMinerals() > 150 + (100 * n_gateway))
+            {
+                // Build gateway near pylon
+                TryBuildStructureNearPylon(ABILITY_ID::BUILD_GATEWAY, UNIT_TYPEID::PROTOSS_PROBE);
+                return;
+            }
+        }
+    }
+    
+    if (n_cybernetics > 0 && n_forge < 2)
+    {
+        // Build forge near pylon
+        TryBuildStructureNearPylon(ABILITY_ID::BUILD_FORGE, UNIT_TYPEID::PROTOSS_PROBE);
+    }
+
+    else
+    {
+        if (n_gateway > 2 && n_cybernetics > 0)
+        {
+            if (n_robotics_facility < min<size_t>(n_base, 4))
+            {
+                if (observation->GetMinerals() > 200 && observation->GetVespene() > 100)
+                {
+                    // Build robotics facility near pylon
+                    TryBuildStructureNearPylon(ABILITY_ID::BUILD_ROBOTICSFACILITY, UNIT_TYPEID::PROTOSS_PROBE);
+                }
+            }
+        }
+    }
+}
+
+void GoingMerry::OnGameStart() {
+    game_info_ = Observation()->GetGameInfo();
+    cout << "game started." << endl;
+    expansions = search::CalculateExpansionLocations(Observation(), Query());
+
+    //Temporary, we can replace this with observation->GetStartLocation() once implemented
+    startLocation = Observation()->GetStartLocation();
+    stagingLocation = startLocation;
+}
+
+void GoingMerry::OnStep() 
+{ 
+    const ObservationInterface* observation = Observation();
+
+    //Throttle some behavior that can wait to avoid duplicate orders.
+    int frames_to_skip = 4;
+    if (observation->GetFoodUsed() >= observation->GetFoodCap()) {
+        frames_to_skip = 6;
+    }
+
+    if (observation->GetGameLoop() % frames_to_skip != 0) {
+        return;
+    }
+
+    BuildOrder();
+
+    if (TryBuildPylon()) {
+        return;
+    }
+
+    if (TryBuildExpansionNexus()) {
+        return;
+    }
+}
+
+bool GoingMerry::TryBuildStructureNearPylon(AbilityID ability_type_for_structure, UnitTypeID) {
+    const ObservationInterface* observation = Observation();
+
+    //Need to check to make sure its a pylon instead of a warp prism
+    std::vector<PowerSource> power_sources = observation->GetPowerSources();
+    if (power_sources.empty()) {
+        return false;
+    }
+
+    const PowerSource& random_power_source = GetRandomEntry(power_sources);
+    if (observation->GetUnit(random_power_source.tag) != nullptr) {
+        if (observation->GetUnit(random_power_source.tag)->unit_type == UNIT_TYPEID::PROTOSS_WARPPRISM) {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+    float radius = random_power_source.radius;
+    float rx = GetRandomScalar();
+    float ry = GetRandomScalar();
+    Point2D build_location = Point2D(random_power_source.position.x + rx * radius, random_power_source.position.y + ry * radius);
+    return TryBuildStructure(ability_type_for_structure, UNIT_TYPEID::PROTOSS_PROBE, build_location);
+}
+
+
+// Mine the nearest mineral to Town hall.
+// If we don't do this, probes may mine from other patches if they stray too far from the base after building.
+void GoingMerry::MineIdleWorkers(const Unit* worker, AbilityID worker_gather_command, UnitTypeID vespene_building_type) {
+    const ObservationInterface* observation = Observation();
+    Units bases = observation->GetUnits(Unit::Alliance::Self, IsTownHall());
+    Units geysers = observation->GetUnits(Unit::Alliance::Self, IsUnit(vespene_building_type));
+
+    const Unit* valid_mineral_patch = nullptr;
+
+    if (bases.empty()) {
+        return;
+    }
+
+    for (const auto& geyser : geysers) {
+        if (geyser->assigned_harvesters < geyser->ideal_harvesters) {
+            Actions()->UnitCommand(worker, worker_gather_command, geyser);
+            return;
+        }
+    }
+    //Search for a base that is missing workers.
+    for (const auto& base : bases) {
+        //If we have already mined out here skip the base.
+        if (base->ideal_harvesters == 0 || base->build_progress != 1) {
+            continue;
+        }
+        if (base->assigned_harvesters < base->ideal_harvesters) {
+            valid_mineral_patch = FindNearestMineralPatch(base->pos);
+            Actions()->UnitCommand(worker, worker_gather_command, valid_mineral_patch);
+            return;
+        }
+    }
+
+    if (!worker->orders.empty()) {
+        return;
+    }
+
+    //If all workers are spots are filled just go to any base.
+    const Unit* random_base = GetRandomEntry(bases);
+    valid_mineral_patch = FindNearestMineralPatch(random_base->pos);
+    Actions()->UnitCommand(worker, worker_gather_command, valid_mineral_patch);
+}
+
+void GoingMerry::OnUnitIdle(const Unit* unit) {
+    switch (unit->unit_type.ToType()) {
+    case UNIT_TYPEID::PROTOSS_PROBE: {
+        MineIdleWorkers(unit, ABILITY_ID::HARVEST_GATHER, UNIT_TYPEID::PROTOSS_ASSIMILATOR);
+        break;
+    }
+    case UNIT_TYPEID::PROTOSS_CYBERNETICSCORE: {
+        const ObservationInterface* observation = Observation();
+        Units nexus = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::PROTOSS_NEXUS));
+    }
+    default:
+        break;
+    }
+}
+
+bool GoingMerry::TryBuildPylon() {
     const ObservationInterface* observation = Observation();
 
     // If we are not supply capped, don't build a supply depot.
-    if (observation->GetFoodUsed() <= observation->GetFoodCap() - 2)
+    if (observation->GetFoodUsed() < observation->GetFoodCap() - 6) {
         return false;
+    }
 
-    // Try and build a depot. Find a random SCV and give it the order.
-    return TryBuildStructure(ABILITY_ID::BUILD_SUPPLYDEPOT);
+    if (observation->GetMinerals() < 100) {
+        return false;
+    }
+
+    //check to see if there is already on building
+    Units units = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::PROTOSS_PYLON));
+    if (observation->GetFoodUsed() < 40) {
+        for (const auto& unit : units) {
+            if (unit->build_progress != 1) {
+                return false;
+            }
+        }
+    }
+
+    // Try and build a pylon. Find a random Probe and give it the order.
+    float rx = GetRandomScalar();
+    float ry = GetRandomScalar();
+    Point2D build_location = Point2D(stagingLocation.x + rx * 15, stagingLocation.y + ry * 15);
+    return TryBuildStructure(ABILITY_ID::BUILD_PYLON, UNIT_TYPEID::PROTOSS_PROBE, build_location);
+}
+
+size_t GoingMerry::CountUnitType(UNIT_TYPEID unit_type) {
+    return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
 }
 
 const Unit* GoingMerry::FindNearestMineralPatch(const Point2D& start) {
@@ -108,14 +349,34 @@ const Unit* GoingMerry::FindNearestMineralPatch(const Point2D& start) {
     return target;
 }
 
-bool GoingMerry::TryBuildBarracks() {
+//Expands to nearest location and updates the start location to be between the new location and old bases.
+bool GoingMerry::TryExpand(AbilityID build_ability, UnitTypeID worker_type) {
     const ObservationInterface* observation = Observation();
-    if (CountUnitType(UNIT_TYPEID::TERRAN_SUPPLYDEPOT) < 1) {
-        return false;
-    }
-    if (CountUnitType(UNIT_TYPEID::TERRAN_BARRACKS) > 0) {
-        return false;
-    }
+    float minimum_distance = std::numeric_limits<float>::max();
+    Point3D closest_expansion;
+    for (const auto& expansion : expansions) {
+        float current_distance = Distance2D(startLocation, expansion);
+        if (current_distance < .01f) {
+            continue;
+        }
 
-    return TryBuildStructure(ABILITY_ID::BUILD_BARRACKS);
+        if (current_distance < minimum_distance) {
+            if (Query()->Placement(build_ability, expansion)) {
+                closest_expansion = expansion;
+                minimum_distance = current_distance;
+            }
+        }
+    }
+    //only update staging location up till 3 bases.
+    if (TryBuildStructure(build_ability, worker_type, closest_expansion, true) && observation->GetUnits(Unit::Self, IsTownHall()).size() < 4) {
+        stagingLocation = Point3D(((stagingLocation.x + closest_expansion.x) / 2), ((stagingLocation.y + closest_expansion.y) / 2),
+            ((stagingLocation.z + closest_expansion.z) / 2));
+        return true;
+    }
+    return false;
+
+}
+
+void GoingMerry::OnGameEnd() {
+    std::cout << "Game Ended for: " << std::to_string(Control()->Proto().GetAssignedPort()) << std::endl;
 }
--- GoingMerry.h
+++ GoingMerry.h
@@ -8,20 +8,58 @@
 #include "sc2utils/sc2_manage_process.h"
 #include "sc2utils/sc2_arg_parser.h"
 
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
 using namespace sc2;
+using namespace std;
 
 class GoingMerry : public Agent {
 public:
 	void OnGameStart();
+
 	void OnStep();
+
 	void OnUnitIdle(const Unit* unit);
 
 private:
+
+	int target_worker_count = 15;
+	int max_worker_count = 70;
+	GameInfo game_info_;
+	vector<Point3D> expansions;
+	Point3D startLocation;
+	Point3D stagingLocation;
+	string last_action_text_;
+
+	bool TryBuildPylon();
+
+	int GoingMerry::GetExpectedWorkers(UNIT_TYPEID vespene_building_type);
+
+	size_t CountUnitType(const ObservationInterface* observation, UnitTypeID unit_type);
+
+	void BuildOrder();
+
+	bool TryExpand(AbilityID build_ability, UnitTypeID worker_type);
+
 	size_t CountUnitType(UNIT_TYPEID unit_type);
-	bool GoingMerry::TryBuildStructure(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type = UNIT_TYPEID::TERRAN_SCV);
+
+	bool TryBuildStructureNearPylon(AbilityID ability_type_for_structure, UnitTypeID unit_type);
+
+	bool TryBuildStructure(AbilityID ability_type_for_structure, UnitTypeID unit_type, Tag location_tag);
+
+	bool GoingMerry::TryBuildStructure(AbilityID ability_type_for_structure, UnitTypeID unit_type, Point2D location, bool isExpansion = false);
+
 	bool TryBuildSupplyDepot();
+
 	const Unit* FindNearestMineralPatch(const Point2D& start);
-	bool TryBuildBarracks();
+
+	bool TryBuildExpansionNexus();
+
+	void GoingMerry::OnGameEnd();
+
+	void GoingMerry::MineIdleWorkers(const Unit* worker, AbilityID worker_gather_command, UnitTypeID vespene_building_type);
 };
 
 #endif
