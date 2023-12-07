e5b77ffc193cd24c65793d294a9ac71555bbb445
David Parise <parise@ualberta.ca>
2023-12-06 21:02:59 -0700

    Update main.cpp
    Undid changes made for testing

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 #include "GoingMerry.h"
 #include "LadderInterface.h"
-#include <bits.h>
 
 using namespace sc2;
 using namespace std;
@@ -17,32 +16,11 @@ using namespace std;
 
 #pragma region Helper functions
 
-void getStats(vector<int> results, int* stats)
-{
-    for (const auto& result : results)
-    {
-        stats[result] += 1;
-    }
-}
-
-void printStats(int* stats, int n, int num_games)
-{
-    float win_percent = (float)stats[0] / (float)num_games;
-    float loss_percent = (float)stats[1] / (float)num_games;
-    float draws_percent = (float)stats[2] / (float)num_games;
-    float undecided_percent = (float)stats[3] / (float)num_games;
-
-    cout << "Number of wins: " << stats[0] << ", Win percentage: " << win_percent*100 << "%" << endl;
-    cout << "Number of losses: " << stats[1] << ", Loss percentage: " <<  loss_percent*100 << "%" << endl;
-    cout << "Number of draws: " << stats[2] << ", Draw percentage: " << draws_percent*100 << "%" << endl;
-    cout << "Number of undecided: " << stats[3] << ", Undecided percentage: " << undecided_percent*100 << "%" << endl;
-}
-
 #pragma endregion
 
 int main(int argc, char* argv[]) {
 
-    RunBot(argc, argv, new GoingMerry(), sc2::Race::Protoss);
+	RunBot(argc, argv, new GoingMerry(), sc2::Race::Protoss);
     
 	return 0;
 }
